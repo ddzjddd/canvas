ec03a80b506a55fa71bf214452e55da9d94c20fc
ddzjddd <1102327209@qq.com>
2026-03-02 14:45:14 +0800

    chore: remove python implementation files

--- app/workflow_engine/context.py
+++
@@ -1,5 +0,0 @@
-def build_template_context(run_inputs: dict, node_outputs: dict) -> dict:
-    return {
-        "inputs": run_inputs,
-        **{nid: {"outputs": outputs} for nid, outputs in node_outputs.items()},
-    }
